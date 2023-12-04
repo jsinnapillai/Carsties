8bc85420e754b74041ed269bd9fe5059c8a31f15
jsinnapillai <jsinnapillai@gmail.com>
2023-12-04 17:59:50 +0000

    end of session two

---
+++ src/AuctionService/Data/AuctionDBContext.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AuctionService.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuctionService.Data
+{
+    public class AuctionDBContext : DbContext
+    {
+        public AuctionDBContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Auction> Auctions { get; set; }
+        // public DbSet<Item> Items { get; set; }
+    }
+}
